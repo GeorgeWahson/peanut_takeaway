6d0cf206951caeaf84e41894def928c60dd42cc6
GeorgeWahson <georgewahson@qq.com>
2022-09-23 16:32:16 +0800

    test

---
+++ src/test/java/com/wahson/PeanutTakeawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wahson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeanutTakeawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
