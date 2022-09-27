69caa248ed3f754c22168e9d6777a5dfd944ab6e
GeorgeWahson <georgewahson@qq.com>
2022-09-27 18:42:14 +0800

    测试类

--- src/test/java/com/wahson/PeanutTakeawayApplicationTests.java
+++ src/test/java/com/wahson/PeanutTakeawayApplicationTests.java
@@ -1,5 +1,6 @@
 package com.wahson;
 
+import com.wahson.util.SMSUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PeanutTakeawayApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testFileLoad() {
+        SMSUtils.sendMessage(null,null, null, "1234");
     }
 
 }
