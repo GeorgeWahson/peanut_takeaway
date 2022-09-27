5bdd718273afe34529d5fa8ba022857fda3b97a1
GeorgeWahson <georgewahson@qq.com>
2022-09-27 18:39:35 +0800

    mapper接口实现mybatis-plus

---
+++ src/main/java/com/wahson/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.wahson.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wahson.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
